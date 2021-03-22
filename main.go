9cc684b21ca569e18dfe9aa3b8930c6c444274e5
abhilashaam <abhilashaamgowda@gmail.com>
2021-03-22 10:49:29 +0530

    first update3

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Printf("hey")
-	fmt.Printf("hey")
-	fmt.Printf("hey")
+	fmt.Printf("commit1")
+	fmt.Printf("commit2")
+	fmt.Printf("commit3")
+	fmt.Printf("commit")
 }
